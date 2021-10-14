5be1ffdb4b18f00716c7805b9a0436944ff6360a
Stefania Pirrotta <s.pirrotta.sp@gmail.com>
2021-10-14 10:31:19 +0200

    create signatureTable documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Table of all available signatures.
+#'
+#' A table containing main information of all the available signatures in the package.
+#'
+#' @format A data frame with 18 rows and 6 variables:
+#' \describe{
+#'   \item{functionName}{names of all the available functions}
+#'   \item{tumorTissue}{tumor tissue for which the signature was created}
+#'   \item{signatureType}{main topic of the signature}
+#'   \item{author}{first author of the signature paper}
+#'   \item{reference}{reference of the signature paper}
+#'   \item{datasetInput}{kind of input data for which the signature was created}
+#'   ...
+#' }
+"signatureTable"
