ceaf1af57c994e153420f4a96765d5da798c03b1
Stefania Pirrotta <s.pirrotta.sp@gmail.com>
2022-06-14 18:05:40 +0200

    adjust notes from check

--- R/fakeData.R
+++ R/fakeData.R
@@ -1,11 +1,12 @@
+#' @importFrom stats rpois rnorm
 fakeData <- function(sname, input = "rnaseq") {
     g <- GetGenes(sname)
     g <- g[, 1]
     n <- length(g) * 5
     if (input == "rnaseq") {
-        rmatrix <- matrix(stats::rpois(n, 100), ncol = 5)
+        rmatrix <- matrix(rpois(n, 100), ncol = 5)
     } else if (input == "microarray") {
-        rmatrix <- matrix(stats::rnorm(n, 100, 50), ncol = 5)
+        rmatrix <- matrix(rnorm(n, 100, 50), ncol = 5)
     }
     rownames(rmatrix) <- g
     return(rmatrix)
