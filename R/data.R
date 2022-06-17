95529f1aa9ba17a809343f4338c0a1a5098586e6
Stefania Pirrotta <s.pirrotta.sp@gmail.com>
2022-06-17 16:09:13 +0200

    change example data

--- R/data.R
+++ R/data.R
@@ -1,16 +1,6 @@
-#' Table of all available signatures.
+#' Example dataset
 #'
-#' A table containing the main information of all the available
-#' signatures in the package.
+#' This dataset is a subset of the ovarian cancer samples collected inside the
+#' cancer genome atlas (TCGA).
 #'
-#' @format A data frame with 18 rows and 6 variables:
-#' \describe{
-#'   \item{functionName}{names of all the available functions}
-#'   \item{tumorTissue}{tumor tissue for which the signature was created}
-#'   \item{signatureType}{main topic of the signature}
-#'   \item{author}{first author of the signature paper}
-#'   \item{reference}{reference of the signature paper}
-#'   \item{datasetInput}{kind of input data for which the signature was created}
-#'   ...
-#' }
-"signatureTable"
+"subovse"
