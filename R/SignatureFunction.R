cc7f2e71a9a59e06be3041565184ca2ac5e95952
Anna Bortolato <anna.bortolato@unipd.it>
2023-11-10 17:22:36 +0100

    SignatureFunction: corrected log2 in EMT_Thompson

--- R/SignatureFunction.R
+++ R/SignatureFunction.R
@@ -119,8 +119,8 @@ EMTSign <- function(
               "EMTSign", datasetm, Sign_M, "mesenchymal", author = author)
             
             t_dataset<-t(datasetm)
-            epi<-rowSums(scale(log2(t_dataset[ ,intersect(Sign_E, colnames(t_dataset))])))
-            mes<-rowSums(scale(log2(t_dataset[ ,intersect(Sign_M, colnames(t_dataset))])))
+            epi<-rowSums(scale(log2(t_dataset[ ,intersect(Sign_E, colnames(t_dataset))]+1)))
+            mes<-rowSums(scale(log2(t_dataset[ ,intersect(Sign_M, colnames(t_dataset))]+1)))
             score<-mes-epi
             }
         return(.returnAsInput(
