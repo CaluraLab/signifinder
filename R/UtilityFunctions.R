b9ffd7d17a9fd158957a4e887a17fac4768164b2
Martina Aere <martina.aere@gmail.com>
2024-04-22 16:08:08 +0200

    Fix SCSubtype_Wu

--- R/UtilityFunctions.R
+++ R/UtilityFunctions.R
@@ -172,7 +172,7 @@ my_colors <- colorRampPalette(my_colors)(100)
       "SCSubtype_Wu_Basal", "SCSubtype_Wu_Her2E",
       "SCSubtype_Wu_LumA", "SCSubtype_Wu_LumB"
     )) {
-      sname <- substring(name, 16)
+      sname <- substring(name, 14)
       g <- SCSubtype_Wu$SYMBOL[SCSubtype_Wu$class == sname]
     } else if (name %in% c(
       "Hypoxia_Barkley", "Stress_Barkley", "Interferon_Barkley", 
